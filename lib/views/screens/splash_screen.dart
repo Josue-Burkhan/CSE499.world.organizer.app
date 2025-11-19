6a1177188e6f7c98b9d48eab28d3c775ce30fd60
joshua.burkhan <bai21030@byui.edu>
2025-11-19 11:59:34 -0500

    Reflesh Token compleated V1.2

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -63,7 +63,7 @@ class _SplashScreenState extends State<SplashScreen>
           _navigateToWelcome();
         }
       } catch (e) {
-        _navigateToWelcome();
+        _navigateToHome();
       }
     } else {
       _navigateToWelcome();
